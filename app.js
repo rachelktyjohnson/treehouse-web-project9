748508cfc9928a133300ae7ccd322b4c74f0c13f
Rachel Johnson <rachelktyjohnson@gmail.com>
2021-07-17 22:44:02 +1000

    Set up routes and middleware

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const data = require('./data.json');
+
+app.set('view engine', 'pug')
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index', {data: data.projects})
+})
+
+app.get('/about',(req, res)=>{
+    res.render('about');
+})
+
+app.get('/project/:id',(req, res)=>{
+    res.render('project', {data: data.projects[req.params.id]})
 })
 
 app.listen(port, () => {
